5ee5f4f7beaa2afc9f2860a31198060bf19e5577
chenshuai <117797954+mini-zoro@users.noreply.github.com>
2023-12-03 02:53:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,53 @@
-//create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-//create database
-const Datastore = require('nedb');
-const db = new Datastore('comments.db');
-db.loadDatabase();
-//allow express to use static files
-app.use(express.static('public'));
-//allow express to use json
-app.use(express.json({limit: '1mb'}));
-//get request
-app.get('/api', (request, response) => {
-    db.find({}, (err, data) => {
-        if(err) {
-            response.end();
-            return;
+// Create web server
+
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Comment = require("./models/comment");
+var seedDB = require("./seeds");
+
+mongoose.connect("mongodb://localhost/yelp_camp_v4");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+seedDB();
+
+// Campground schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    // Create a reference to the comments
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
         }
-        response.json(data);
-    });
+    ]
 });
-//post request
-app.post('/api', (request, response) => {
-    console.log('I got a request!');
-    console.log(request.body);
-    const data = request.body;
-    const timestamp = Date.now();
-    data.timestamp = timestamp;
-    db.insert(data);
-    response.json(data);
-});
-//listen for requests
-app.listen(port, () => {
-    console.log(`Starting server at ${port}`);
-});
+
+// Create a model using the schema
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "Granite Hill", 
+//         image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg",
+//         description: "This is a huge granite hill. No bathrooms. No water. Beautiful granite!"
+        
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else{
+//             console.log("Newly created campground: ");
+//             console.log(campground);
+//         }
+//     });
+
+// var campgrounds = [
+//         {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+//         {name: "Granite Hill", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},
+//         {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+//         {name: "Salmon Creek", image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg"},
+//         {name: "Granite Hill", image: "https://farm9.staticflickr.com/8673/15989950903_8185ed97c3.jpg"},
+//         {name: "Mountain Goat's Rest", image
